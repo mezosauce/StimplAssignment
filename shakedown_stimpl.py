ad512fbdf8e7e3bdbef3076f7f21b2057a742d42
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-11-09 20:01:38 -0500

    Delete shakedown_stimpl.py

--- shakedown_stimpl.py
+++
@@ -1,6 +0,0 @@
-from stimpl.expression import *
-from stimpl.runtime import *
-
-if __name__=='__main__':
-  program = Print(Assign(Variable("i"), StringLiteral("Hello, World")))
-  run_stimpl(program)
